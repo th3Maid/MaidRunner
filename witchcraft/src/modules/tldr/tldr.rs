74bcce1f6cd9a572607029a90b4879f9ec0c7b11
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2025-01-09 01:31:15 -0300

    Added tldr page to witchcraft

--- witchcraft/src/modules/tldr/tldr.rs
+++ witchcraft/src/modules/tldr/tldr.rs
@@ -1,10 +1,23 @@
-use crate::{core::types::Closure, search_value};
+use std::path::Path;
+
+use crate::core::consts::WITCH_SPELLS_ROOT_DIR;
+use crate::core::core::*;
+use crate::core::types::Closure;
 use termimad::*;
 
 pub fn read_markdown_from_path(argsv: &[String]) -> i32 {
-    let skin = MadSkin::default();
-    skin.print_text(&std::fs::read_to_string("../README.md").unwrap());
-    return 0;
+    let command = search_value("page", argsv);
+    let path = &format!("{}pages", WITCH_SPELLS_ROOT_DIR);
+    let pages_paths = directory_lookup(Path::new(path));
+    for page in pages_paths {
+        if page.contains(&format!("/{}.md", command)) {
+            let skin = MadSkin::default();
+            skin.print_text(&std::fs::read_to_string(page).unwrap());
+            return 0;
+        }
+    }
+
+    return 2;
 }
 
 pub fn api() -> Closure {
