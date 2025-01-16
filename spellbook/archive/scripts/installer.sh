3dad6dd015d4a7de9c01506cdbd9fb7ee4f33246
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2025-01-16 02:45:39 -0300

    Added and fixed dataset/db.json sections, evilpages/default/index.html

--- spellbook/archive/scripts/installer.sh
+++ spellbook/archive/scripts/installer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 echo && echo "Installer"
-sudo cp -nr spellbook/ /var/
+sudo cp -r --update=none spellbook/ /var/
 sudo chown -R $(whoami):$(whoami) /var/spellbook
+sudo apt install -y chromium-browser
 echo 'export WITCH_SPELLS_ROOT_DIR=/var/spellbook/archive/' >> ~/.bash_profile
 export WITCH_SPELLS_ROOT_DIR=/var/spellbook/archive/
 sudo cp -r witchcraft /bin/witchcraft
